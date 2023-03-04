4b9bb824d92a0a60ca7fc50ad6dfed40266d1ca8
Dustin-11 <[11kurtzd@gmail.com]>
2023-03-04 08:45:31 -0500

    Solved Problem #1

--- script.js
+++ script.js
@@ -1,2 +1,14 @@
 /*This is a workspace to peform Code Coach practice exercises offered through the Sololearn app*/
 //For some reason they do not allow for JavaScript solutions
+
+function calc(houses) {
+if (houses >= 3) {
+    let perc = (2/houses) * 100; 
+    Math.round(perc)
+    console.log(perc);
+}
+}
+
+calc(10);
+calc(20);
+calc(4);
